8a36fb513eb2e9314ccd3aee426821aa6d074c72
Susmitha Duddukuri <duddukurisusmitha20@gmail.com>
2026-03-26 19:57:20 +0530

    Updated UI

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ h1, h2, h3, h4, h5, h6, p, span, div {
 }
 
 input, textarea, select, .stSelectbox, .stTextInput, .stTextArea {
-    color: black !important;
+    color: white !important;
     background-color: white !important;
 }
 
